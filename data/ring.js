fd527fa6a2570b8a614d61fa2c1be9250abddd6f
Adron Hall <adronhall@gmail.com>
2014-06-19 10:27:30 -0700

    Adding data ring (that's what I'm calling it) for data tier code.

---
+++ data/ring.js
@@ -0,0 +1,17 @@
+/**
+ * Created by adron on 6/19/14.
+ */
+
+var orchestrator = require('orchestrate');
+var collection = 'deconstructed';
+var groups = 'accounts.groups';
+var users = 'accounts.users';
+var apikeys = 'accounts.apikeys';
+
+var ring = {};
+
+ring.blagh = function () {
+
+}
+
+module.exports = ring;
