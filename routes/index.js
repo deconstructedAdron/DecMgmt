3dd1bcff40ace2ceb7302c4452c1fed9d4a3ba79
Adron Hall <adronhall@gmail.com>
2014-06-19 09:49:02 -0700

    Adding parameter to verify

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+
+    var apikey = process.env.ORCHESTRATE_API_KEY;
+
+    res.render('index', { title: 'Express + ' + apikey });
 });
 
 module.exports = router;
