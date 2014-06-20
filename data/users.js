a6bcec2b531f0204880a85e9e46d18c8279d8abb
Adron Hall <adronhall@gmail.com>
2014-06-19 17:00:42 -0700

    Not cleanup, sort of, kind of.

--- data/users.js
+++ data/users.js
@@ -5,17 +5,18 @@
 
 
 var users = {};
-module.exports = users;
 
+users.blaghdblagh = function blaghdWoah() {
 
-var users_sample = {};
+}
+
 // sample of what should/needs to match for the api token to work with things...
 // to be deleted and replaced with real codes.
-users.getUsers = function getUsers() {
+users.getUsers = function () {
     var users_sample_stuffs = [
         { id: 2, username: 'adron', name: 'Adron Hall', password: 'hgalb!', email: 'adron@deconstructed.io'},
         { id: 3, username: 'public', password: 'blagh', email: 'public@deconstructed.io' }
     ];
     return users_sample_stuffs;
 }
-module.exports = users_sample;
+module.exports = users;
