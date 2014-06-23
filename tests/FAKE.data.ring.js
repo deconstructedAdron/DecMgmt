6c114bf786e87d8b201bf046e37d0e3cdcf0da19
Adron Hall <adronhall@gmail.com>
2014-06-23 14:47:20 -0700

    Got the fake data ring with a minor change and added docs from wikipedia into the test_routes.

--- tests/FAKE.data.ring.js
+++ tests/FAKE.data.ring.js
@@ -15,14 +15,15 @@ FAKE_RING.search = function (collection, query) {
 }
 
 FAKE_RING.kv_get = function (collection, key) {
-    if(collection === users)
+    if (collection === users)
 
 
 
-    return 'kv_get';
+        return 'kv_get';
 }
 
 FAKE_RING.kv_set = function (collection, key, value) {
+
     return 'kv_set';
 }
 
