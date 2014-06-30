19c3d33d77bceed3e9ca01491bf40d4ae0d50ff1
Adron Hall <adronhall@gmail.com>
2014-06-29 21:57:12 -0700

    Adding back models tests for roles and users.

--- tests/unit/models.roles.js
+++ tests/unit/models.roles.js
@@ -3,7 +3,7 @@
  *Description: basic tests for the roles DTO/Business Logic.
  */
 
-var Roles = require('../models/roles');
+var Roles = require('../../models/roles');
 
 describe('The business logic roles', function () {
     it('should have 5 different roles.', function () {
--- tests/unit/models.users.js
+++ tests/unit/models.users.js
@@ -2,23 +2,21 @@
  * Created by adronhall on 6/20/14.
  * Description:
  */
-var Users = require('../models/users');
-var FAKE_RING = require('FAKE.data.ring.js');
-var users = new Users(FAKE_RING);
+var Users = require('../../models/users');
+var data_ring = require('../../data/ring');
+var users = new Users(data_ring);
 
 describe('The business logic for users', function () {
     it('should exist when instantiated with a ring connection.', function () {
         users.should.exist;
     });
     it('should have a connection that is the same as the passed connection.', function () {
-        users.ConnectionRing.should.eql(FAKE_RING);
+        users.ConnectionRing.should.eql(data_ring);
     });
 })
 
 describe('The users to authenticate', function () {
-
     // { id: 2, username: 'adron', name: 'Adron Hall', password: 'hgalb!', email: 'adron@deconstructed.io'},
-
 
 //    it('should return a user record to auth with a username, password and appropriate properties.', function () {
 //        users.authByUserPassword().id.should.exist;
@@ -28,7 +26,6 @@ describe('The users to authenticate', function () {
 
 describe('The users to verify API key', function () {
     // { id: 2, username: 'adron', name: 'Adron Hall', password: 'hgalb!', email: 'adron@deconstructed.io'},
-
 
 //    it('should return a user API key list similar to that above.', function () {
 //        users.authByUserPassword().id.should.exist;
